268a08746dbede0987451754597a7b3b72eaf3aa
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-11-04 10:58:09 -0400

    test ln_simpson and make atol smol

--- jax_ext/integrate/tests/test_simpson.py
+++ jax_ext/integrate/tests/test_simpson.py
@@ -1,8 +1,11 @@
+import jax
 import jax.numpy as jnp
 import pytest
 from scipy.integrate import simpson as scipy_simpson
 
-from ..simpson import simpson
+from ..simpson import ln_simpson, simpson
+
+jax.config.update("jax_enable_x64", True)
 
 
 @pytest.mark.parametrize("N", [2, 128, 129])
@@ -13,4 +16,15 @@ def test_simpson(N):
     result = simpson(y, x)
     expected = scipy_simpson(y, x)
 
-    assert jnp.allclose(result, expected)
+    assert jnp.allclose(result, expected, atol=1e-16)
+
+
+@pytest.mark.parametrize("N", [2, 128, 129])
+def test_ln_simpson(N):
+    x = jnp.linspace(0., 1., N)
+    y = jnp.sin(x)**2 + 0.341  # random positive function
+
+    result = ln_simpson(jnp.log(y), x)
+    expected = jnp.log(scipy_simpson(y, x))
+
+    assert jnp.allclose(result, expected, atol=1e-15)
